f037d9a195097bf1bd3176f4edaa0edeac92f088
James Lezzer <jflezzer@gmail.com>
2016-10-10 09:33:12 -0400

    Chapter 13 homework for submission

--- hw1301.py
+++ hw1301.py
@@ -1,24 +1,19 @@
 import urllib
 import xml.etree.ElementTree as ET
 
-serviceurl = 'http://python-data.dr-chuck.net/comments_42.xml'
-
 while True:
-    address = serviceurl
-    #raw_input('Enter location: ')
+    address = raw_input('Enter location: ')
     if len(address) < 1 : break
-
-    url = serviceurl
+    url = address
     print 'Retrieving', url
     uh = urllib.urlopen(url)
     data = uh.read()
     print 'Retrieved',len(data),'characters'
-    print data
     tree = ET.fromstring(data)
-
-    for item in data:
-        count = tree.find('count').text
-        print count
-        
-    #results = tree.findall('.//count').text
-    #print results
+    #convert the data to a list for all of the comments
+    lst = tree.findall('.//comment')
+    print 'Count:', len(lst)
+    summary = 0
+    for item in lst:
+        summary = int(item.find('count').text) + summary
+    print 'Sum:',summary
